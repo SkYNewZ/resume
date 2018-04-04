f766e155f8b000fd379ac24d2f129969293322c2
Quentin Lemaire <quentin.lemaire10@free.fr>
2018-04-04 18:57:06 +0200

    Initial commit

---
+++ src/lang/fr.js
@@ -0,0 +1,11 @@
+const lang = {
+  contact: 'Contact',
+  born: 'Né en',
+  bornIn: 'à',
+  experience: 'Expériences professionelles',
+  education: 'Formation',
+  skills: 'Compétences',
+  about: 'À propos de moi',
+  hobbies: 'Loisirs'
+};
+export default lang;
